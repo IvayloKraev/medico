8e793b0f98775320f46803cea40e508bb80d3c33
IvayloKraev <ivaylokraev@protonmail.com>
2024-10-29 09:28:28 +0200

    Use validate interface in the auth controller

--- controllers/auth.go
+++ controllers/auth.go
@@ -7,9 +7,19 @@ import (
 )
 
 func SignIn(c *fiber.Ctx) error {
-	signInData := new(dto.SignIn)
+	signInData := new(dto.CommonUserSignIn)
 
 	if err := c.BodyParser(signInData); err != nil {
+
+		fmt.Println(err)
+
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"err": true,
+			"msg": err.Error(),
+		})
+	}
+
+	if err := signInData.Validate(); err != nil {
 
 		fmt.Println(err)
 
