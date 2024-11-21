0f51a050234e919a212a370c0762f5aee12db4ff
IvayloKraev <ivaylokraev@protonmail.com>
2024-11-21 08:29:13 +0200

    Make the errors public and uncapitalize them

--- errors/auth.go
+++ errors/auth.go
@@ -1,16 +1,16 @@
 package medicoErrors
 
 const (
-	invalidEmail    = "INVALID EMAIL"
-	invalidPassword = "INVALID PASSWORD"
+	InvalidEmail    = "INVALID EMAIL"
+	InvalidPassword = "INVALID PASSWORD"
 )
 
 const (
-	incorrectEmail         = "The provided email is not correct"
-	notEnoughLowerCase     = "Password must contain at least 2 lower case letters"
-	notEnoughUpperCase     = "Password must contain at least 2 upper case letters"
-	notEnoughDigits        = "Password must contain at least 2 digits"
-	notEnoughSpecialChars  = "Password must contain at least 2 special characters"
-	notEnoughNumberOfChars = "Password must be at least 12 characters"
-	includedWhiteSpace     = "Password must not include spaces"
+	IncorrectEmail         = "the provided email is not correct"
+	NotEnoughLowerCase     = "password must contain at least 2 lower case letters"
+	NotEnoughUpperCase     = "password must contain at least 2 upper case letters"
+	NotEnoughDigits        = "password must contain at least 2 digits"
+	NotEnoughSpecialChars  = "password must contain at least 2 special characters"
+	NotEnoughNumberOfChars = "password must be at least 12 characters"
+	IncludedWhiteSpace     = "password must not include spaces"
 )
