5aecfbb93313cf90c7a98cb67b28ecb3a7040178
IvayloKraev <ivaylokraev@protonmail.com>
2024-11-27 21:55:30 +0200

    Add custom types for Email and Password

--- dto/common.go
+++ dto/common.go
@@ -1,5 +1,34 @@
 package dto
 
+import (
+	"errors"
+	"fmt"
+	"medico/errors"
+	"medico/validators"
+)
+
 type IValidate interface {
 	Validate() error
+}
+
+type Email string
+type Password string
+
+func (email *Email) Validate() error {
+	result := validators.ValidateEmail(string(*email))
+
+	if result != nil {
+		return errors.New(fmt.Sprintf("%s - %s", medicoErrors.InvalidEmail, medicoErrors.IncorrectEmail))
+	}
+
+	return nil
+}
+
+func (password *Password) Validate() error {
+	lowerCaseResult := validators.ValidateNumberOfLowerCase(string(*password))
+
+	if lowerCaseResult != nil {
+	}
+
+	return errors.ErrUnsupported
 }
