b24c04dc18c8a73641bb8c6932dd64af480e36be
IvayloKraev <ivaylokraev@protonmail.com>
2025-01-12 12:20:55 +0200

    Use the new code in medico main file

--- medico.go
+++ medico.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"medico/config"
 	"medico/db"
 	"medico/routes"
 )
 
 func main() {
+	databaseConfig := config.LoadDatabaseConfig()
+
+	mainRepository := db.CreateNewRepository("main", databaseConfig)
+
+	db.Migrate(mainRepository)
+
 	medicoFiber := fiber.New()
 
 	routes.SetUpRoutes(medicoFiber)
 
-	db.Connect()
-	medicoFiber.Listen("0.0.0.0:3000")
+	_ = medicoFiber.Listen("0.0.0.0:3000")
 }
