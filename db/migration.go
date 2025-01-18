55d96b2d73d6aa1eb2eaa0ffb90e173e20a7d401
IvayloKraev <ivaylokraev@protonmail.com>
2025-01-18 22:41:37 +0200

    Add structures in the migrator

--- db/migration.go
+++ db/migration.go
@@ -1,13 +1,111 @@
 package db
 
-import "medico/data"
+import "medico/models"
 
 func Migrate(repository Repository) {
-	if err := repository.DropTableIfExists(data.CommonUserDB{}); err != nil {
+	if err := repository.DropTableIfExists(models.Unit{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.AuthorizationHolder{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.ActiveIngredient{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.ActiveIngredientInteraction{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.MedicamentApplication{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.Medicament{}); err != nil {
 		return
 	}
 
-	if err := repository.AutoMigrate(data.CommonUserDB{}); err != nil {
+	if err := repository.DropTableIfExists(models.PharmacyBrand{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.PharmacyBranch{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.Pharmacist{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.PharmacyBranchStorage{}); err != nil {
+		return
+	}
+
+	if err := repository.DropTableIfExists(models.Hospital{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.Doctor{}); err != nil {
+		return
+	}
+
+	if err := repository.DropTableIfExists(models.Citizen{}); err != nil {
+		return
+	}
+
+	if err := repository.DropTableIfExists(models.Prescription{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.PrescriptionMedicament{}); err != nil {
+		return
+	}
+	if err := repository.DropTableIfExists(models.PrescriptionState{}); err != nil {
+		return
+	}
+
+	if err := repository.AutoMigrate(models.Unit{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.AuthorizationHolder{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.ActiveIngredient{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.ActiveIngredientInteraction{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.MedicamentApplication{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.Medicament{}); err != nil {
+		return
+	}
+
+	if err := repository.AutoMigrate(models.PharmacyBrand{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.PharmacyBranch{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.Pharmacist{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.PharmacyBranchStorage{}); err != nil {
+		return
+	}
+
+	if err := repository.AutoMigrate(models.Hospital{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.Doctor{}); err != nil {
+		return
+	}
+
+	if err := repository.AutoMigrate(models.Citizen{}); err != nil {
+		return
+	}
+
+	if err := repository.AutoMigrate(models.Prescription{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.PrescriptionMedicament{}); err != nil {
+		return
+	}
+	if err := repository.AutoMigrate(models.PrescriptionState{}); err != nil {
 		return
 	}
 }
