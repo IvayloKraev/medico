685238c5c31a4ec0a97661784e1ed7083aeb3b68
IvayloKraev <ivaylokraev@protonmail.com>
2025-02-15 17:41:14 +0200

    Create repo for admin operations

--- repo/admin.go
+++ repo/admin.go
@@ -1,12 +1,16 @@
 package repo
 
 import (
+	"github.com/google/uuid"
 	"medico/config"
 	"medico/models"
 )
 
 type AdminRepo interface {
 	FindAuthByEmail(email string, adminAuth *models.AdminAuth) error
+	CreateModerator(moderatorAuth *models.ModeratorAuth) error
+	DeleteModerator(moderatorId uuid.UUID) error
+	FindAllModerators(moderators *[]models.Moderator) error
 }
 
 type adminRepo struct {
@@ -20,4 +24,16 @@ func NewAdminRepo() AdminRepo {
 
 func (r *adminRepo) FindAuthByEmail(email string, adminAuth *models.AdminAuth) error {
 	return r.repo.First(adminAuth, "email = ?", email).Error
+}
+
+func (r *adminRepo) CreateModerator(moderatorAuth *models.ModeratorAuth) error {
+	return r.repo.Create(moderatorAuth).Error
+}
+
+func (r *adminRepo) DeleteModerator(moderatorId uuid.UUID) error {
+	return r.repo.Where("id = ?", moderatorId.String()).Delete(models.ModeratorAuth{}).Error
+}
+
+func (r *adminRepo) FindAllModerators(moderators *[]models.Moderator) error {
+	return r.repo.Find(moderators).Error
 }
