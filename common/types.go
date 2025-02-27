48e55fa07d47082a6a3f9a3139d52f564f08f864
IvayloKraev <ivaylokraev@protonmail.com>
2025-02-27 16:26:18 +0200

    Move moderator types to new folder

---
+++ common/types.go
@@ -0,0 +1,10 @@
+package common
+
+type ModeratorType string
+
+const (
+	DoctorMod     ModeratorType = "doctorMod"
+	CitizenMod    ModeratorType = "citizenMod"
+	PharmacyMod   ModeratorType = "pharmacyMod"
+	MedicamentMod ModeratorType = "medicamentMod"
+)
